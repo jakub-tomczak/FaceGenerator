a0d52b26831b82e4bf28c2878a431756ce24162e
Jakub Tomczak <jakub.tomczak@live.com>
2019-12-25 09:25:10 +0100

    Fix source dir path.

--- create_dataset_subset.py
+++ create_dataset_subset.py
@@ -6,7 +6,7 @@ from os.path import join, exists
 from os import makedirs
 from tqdm import tqdm
 
-source_dir = join('.', 'datasets', 'celeb_a', 'test')
+source_dir = join('.', 'datasets', 'celeb_a', 'img_align_celeba')
 num_of_images = int(input("number of images: "))
 if not 0 < num_of_images < 200000:
     print("number of images must be in range (0, 200000)")
@@ -16,6 +16,7 @@ target_dir = join('.', 'datasets', 'celeb_a', str(num_of_images))
 if not exists(target_dir):
     makedirs(target_dir)
 
+copied = 0
 try:
     for i in tqdm(range(num_of_images)):
         filename = '{:06d}.jpg'.format(i + 1)
@@ -23,6 +24,7 @@ try:
         target_file = join(target_dir, filename)
         if exists(source_file):
             copyfile(source_file, target_file)
+            copied += 1
 except IOError as e:
     print("Unable to copy file. %s" % e)
     exit(1)
@@ -30,4 +32,4 @@ except:
     print("Unexpected error:", exc_info())
     exit(1)
 
-print("\nFile copy done!\n")
+print("\nFile copy done!. Copied {} files.\n".format(copied))
