dfe36644f0162e36e7e9478fecd415210480f5da
Jakub Tomczak <jakub.tomczak@live.com>
2020-01-07 23:10:28 +0000

    Redirect output to outputs directory.

--- run_batch_mode.sh
+++ run_batch_mode.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-filename=$(date +"run_%d_%m_%Y-%T")
-echo 'Launching, output filename $filename'
+dir=outputs
+filename="$dir/$(date +'run_%d_%m_%Y-%T')"
+if [ ! -d $dir ];then
+    mkdir $dir
+fi
+echo "Launching, output filename $filename"
 python3 FaceGenerator.py &> $filename &
-echo "Finished FaceGenerator.py, $(date +"%d_%m_%Y-%T")"
