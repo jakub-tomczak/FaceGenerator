742f581963d82bbc90734b2d32b0c79ff180556c
Jakub Tomczak <jakub.tomczak@live.com>
2020-01-07 23:03:13 +0000

    Redirect the whole output to file.

--- run_batch_mode.sh
+++ run_batch_mode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 filename=$(date +"run_%d_%m_%Y-%T")
 echo 'Launching, output filename $filename'
-python3 FaceGenerator.py > $filename &
+python3 FaceGenerator.py &> $filename &
 echo "Finished FaceGenerator.py, $(date +"%d_%m_%Y-%T")"
